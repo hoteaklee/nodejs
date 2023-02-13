cdae4775764f449f25354e3235ff195c14befb33
hoteaklee <cc18ht@naver.com>
2023-02-13 15:04:35 +0900

    movie.daum - 셀레니엄 2

--- crwaling06b.js
+++ crwaling06b.js
@@ -6,9 +6,8 @@
 
 // 사용할 패키지 가져오기 : require(패키지명)
 const axios = require('axios');     // Ajax 라이브러리
-const cheerio = require('cheerio');   // DOM 라이브러리
 const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
-
+const url =require("url");
 
 async  function main() { // 비동기 I/O 지원 함수 정의
 
@@ -26,41 +25,36 @@ async  function main() { // 비동기 I/O 지원 함수 정의
         //특정 요소가 화면에 위치할때까지 최대 5초간 기다려 줌
         await chrome.wait(until.elementLocated(By.css('.feature_home div:nth-child(3).slide_ranking .tit_item')), 5000);
 
-        // 접속한 사이트의 html 소스를 가져옴
-        const html = await chrome.getPageSource();
-        //console.log(html);
-
-        // 페이지 소스를 dom객체로 변환
-        const dom = cheerio.load(html);
 
         // 영화제목들, 평점, 애매율 추출
-        let movies = dom('.feature_home div:nth-child(3).slide_ranking .tit_item');
-        let rates = dom('.feature_home div:nth-child(3).slide_ranking .txt_num:first-child');
-        let rsrvs = dom('.feature_home div:nth-child(3).slide_ranking .txt_num:last-child');
+        let movies = await chrome.findElements(By.css('.feature_home div:nth-child(3).slide_ranking .tit_item'));
+        let rates =  await chrome.findElements(By.css('.feature_home div:nth-child(3).slide_ranking .txt_num:first-child'));
+        let rsrvs =  await chrome.findElements(By.css('.feature_home div:nth-child(3).slide_ranking .txt_num:last-child'));
 
         //추출한 결과를 저장하기 위한 배열 선언
         let moviess = [], ratess = [], rsrvss = [] ;
 
         // 추출된 영화제목 출력
-        movies.each((indx,movie) => {
-            let title = dom(movie).text().trim()
-            //console.log(title);
-            moviess.push(title);
-        });
+       for (let movie of movies){
+           //console.log(await movie.getText()) ; //getText() 눈에 보이는 요소의 텍스트만 출력
+           let title = await movie.getAttribute('textContent');
+           //console.log(title.trim());
+            moviess.push(title.trim());
+        };
 
        // 추출된 평점 출력
-        rates.each((indx,rate) => {
-            let point = dom(rate).text().trim();
-            //console.log(point);
-            ratess.push(point)
-        });
+        for (let rate of rates){
+            let rateAll = await rate.getAttribute('textContent');
+           // console.log(rateAll);
+            ratess.push(rateAll);
+        };
 
         // 추출된 예매율 출력
-        rsrvs.each((indx,rsrv) => {
-            let rsrt = dom(rsrv).text().trim();
-            //console.log(rsrt);
-            rsrvss.push(rsrt);
-        });
+        for (let rsrv of rsrvs){
+            let rsrvAll = await rsrv.getAttribute('textContent');
+            //console.log(rsrvAll);
+            rsrvss.push(rsrvAll);
+        };
 
         // 한번에 모아서 출력
         for (let i = 0; i < moviess.length; ++i ){
