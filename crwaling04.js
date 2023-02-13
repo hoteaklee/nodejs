e435e8d75fedebac40ed8d133434bc42ba7ec982
hoteaklee <cc18ht@naver.com>
2023-02-13 11:41:11 +0900

    data.co.kr - 코로나19 출력 (xml),등급

--- crwaling04.js
+++ crwaling04.js
@@ -7,34 +7,40 @@
 // 사용할 패키지 가져오기 : require(패키지명)
 const axios = require('axios');     // Ajax 라이브러리
 const cheerio = require('cheerio');   // DOM 라이브러리
+const {XMLParser}= require('fast-xml-parser') ; // xml 처리기 라이브러리
 
 
 async  function main() { // 비동기 I/O 지원 함수 정의
 
     // 접속할 url, 쿼리스트링, user-agent 헤더 지정
+    //인증 vs 인가
     const URL = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty';
     const params = {'serviceKey':'hVrYuNPc8LwRCs5D0D39FUaHss2DrgCmXkcJ9cGWKrIWhctX6HlKm05vxWM7s0ZdjOEDFpaD/r/0OXn1JhfQsg==',
-    'returnType':'json','sidoName':'경기', 'numOfRows':1000, 'ver':1.3
+    'returnType':'xml','sidoName':'서울', 'numOfRows':1000, 'ver':1.3
     };  // serviceKey: 일반인증키(Decoding)
     const headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.78'}
 
 
 
     // axios로 접속해서 html를 블러옴
-    const json = await axios.get(URL,{
+    const xml = await axios.get(URL,{
         params : params, headers : headers
     });  // 서버 용청시 User-Agent 해더 사용
 
     //받아온 데이터 잠시 확인
-   // console.log(json.data)
+    //console.log(xml.data)
 
-    //JSON 으로 불러오기
-    let items = json.data['response']['body']['items'];
-    //console.log(items);
+    // XML을  JSON으로 변환하기
+    const parser = new XMLParser()
+    let json = parser.parse(xml.data);
+
+    // //JSON 으로 불러오기
+     let items = json['response']['body']['items'];
+     //console.log(items);
 
     //미세먼지 정보 출력
     // pm25Value는 출력 안됨!!
-    for (let item of items){
+    for (let item of items['item']){
         console.log(item.sidoName, item.stationName,
             item.pm10Value, item.pm25Value,
             pmGrade(item.pm10Grade), pmGrade(item.pm25Grade),
@@ -42,17 +48,11 @@ async  function main() { // 비동기 I/O 지원 함수 정의
     }
 
 }
-//등급별 이모지
-// 윈도우키 .
+//등급별 이모지( 윈도우키 . )
 // 😍😐😰😱
 
 
 let pmGrade = (val) => {
-    /*let emoji = '😍';
-    if (val == '1') emoji = '😐';
-    else if (val == '2') emoji = '😰';
-    else if (val == '3') emoji = '😱';*/
-
     let emojis= ['😍','😐','😰','😱'];
 
     return emojis[parseInt(val)-1] ;  // 배열 인덱스값으로
